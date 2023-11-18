b3b60de74e47a6208fc4d43413b7cb1ec8fced24
Deras879 <derycard59@gmail.com>
2023-11-17 21:15:50 -0500

    Implementado controller login revisa la BDD

--- Server/src/controllers/login.js
+++ Server/src/controllers/login.js
@@ -1,20 +1,43 @@
-const users = require ("../utils/users");
- 
+// const users = require ("../utils/users");
+const { User } = require("../DB_connection");
 
-const login = (req, res) => {
-    const {email, password} = req.query;
+// const login = (req, res) => {
+//     const {email, password} = req.query;
 
-    if(email && password){
-        const user = users.find((us) => us.email === email && us.password === password)
+//     if(email && password){
+//         const user = users.find((us) => us.email === email && us.password === password)
 
-        if(user){
-            res.status(200).json({access: true})
-        }else{
-            res.status(200).json({access: false})
-        }
-    }else{
-        res.status(400).send("No hay Email o Password")
+//         if(user){
+//             res.status(200).json({access: true})
+//         }else{
+//             res.status(200).json({access: false})
+//         }
+//     }else{
+//         res.status(400).send("No hay Email o Password")
+//     }
+// }
+
+const login = async (req, res) => {
+  const { email, password } = req.query;
+  if (!email || !password) {
+    res.status(400).send("Faltan datos");
+  }
+
+  try {
+    const user = await User.findAll({ where: { email } });
+
+    if (!user[0]) {
+      res.status(404).send("Usuario no encontrado");
+    } else {
+      if (user[0].password !== password) {
+        res.status(403).send("Contraseña incorrecta");
+      } else {
+        res.status(200).json({ acces: true });
+      }
     }
-}
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
 
-module.exports = login
+module.exports = login;
